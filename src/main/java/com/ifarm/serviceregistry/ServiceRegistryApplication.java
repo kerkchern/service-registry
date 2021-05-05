9a5b91d35b6d132829a9950fecd88eefce349748
kc <chuakerkchern@hotmail.com>
2021-05-05 20:32:50 +0800

    a

--- src/main/java/com/ifarm/serviceregistry/ServiceRegistryApplication.java
+++ src/main/java/com/ifarm/serviceregistry/ServiceRegistryApplication.java
@@ -2,7 +2,12 @@ package com.ifarm.serviceregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+
+import com.netflix.appinfo.AmazonInfo;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -12,6 +17,13 @@ public class ServiceRegistryApplication {
 		SpringApplication.run(ServiceRegistryApplication.class, args);
 	}
 	
+//	@Bean
+//	public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils) {
+//		  EurekaInstanceConfigBean b = new EurekaInstanceConfigBean(inetUtils);
+//		  AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
+//		  b.setDataCenterInfo(info);
+//		  return b;
+//	}
 	
 
 }
